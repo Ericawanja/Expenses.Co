4a794b4bc819a223bf3a644554d96827d6c1a379
Ericawanja <wanjaerica@gmail.com>
2023-02-27 16:39:40 +0300

    connected to db and worked on register endpoint

--- Backend/api security/dist/Controllers/index.js
+++ Backend/api security/dist/Controllers/index.js
@@ -1,7 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.register = void 0;
-const register = (req, res) => {
-    res.status(200).json({ msg: 'setting up the server' });
-};
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const dbConnect_1 = require("../helpers/dbConnect");
+const register = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let { id, fullname, email, password, isAdmin = false } = req.body;
+    try {
+        let exists = (yield dbConnect_1.db.execute("getUSer", { email })).length === 0 ? false : true;
+        if (exists)
+            return res.status(400).json({ error: "Wrong registration details" });
+        let hashedPassword = yield bcrypt_1.default.hash(password, 8);
+        yield dbConnect_1.db.execute("registerUser", {
+            id,
+            fullname,
+            email,
+            password: hashedPassword,
+            isAdmin: isAdmin,
+        });
+        res.status(200).json({ info: exists });
+    }
+    catch (error) {
+        let message = error || "Try again later can't process the request now";
+        res.status(500).json({ error: message });
+    }
+});
 exports.register = register;
--- Backend/api security/src/Controllers/index.ts
+++ Backend/api security/src/Controllers/index.ts
@@ -1,5 +1,38 @@
-import express, {Request, Response} from 'express'
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { db } from "../helpers/dbConnect";
 
-export const register = (req:Request, res:Response)=>{
-    res.status(200).json({msg:'setting up the server'})
+interface registerRequest extends Request {
+  body: {
+    id: string;
+    fullname: string;
+    password: string;
+    email: string;
+    isAdmin: Boolean;
+  };
 }
+
+export const register = async (req: registerRequest, res: Response) => {
+  let { id, fullname, email, password, isAdmin = false } = req.body;
+  try {
+    let exists =
+      (await db.execute("getUSer", { email })).length === 0 ? false : true;
+
+    if (exists)
+      return res.status(400).json({ error: "Wrong registration details" });
+
+    let hashedPassword = await bcrypt.hash(password, 8);
+
+    await db.execute("registerUser", {
+      id,
+      fullname,
+      email,
+      password:hashedPassword,
+      isAdmin: isAdmin,
+    });
+    res.status(200).json({ info: exists });
+  } catch (error) {
+    let message = error || "Try again later can't process the request now";
+    res.status(500).json({ error: message });
+  }
+};
