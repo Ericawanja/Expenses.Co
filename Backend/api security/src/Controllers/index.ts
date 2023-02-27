ef1f2856584e59e44db3c444ddff6d4570de2727
Ericawanja <wanjaerica@gmail.com>
2023-02-27 20:53:34 +0300

    register endpoint with validation

--- Backend/api security/src/Controllers/index.ts
+++ Backend/api security/src/Controllers/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { db } from "../helpers/dbConnect";
+import { v4 as uuidv4 } from "uuid";
 
 interface registerRequest extends Request {
   body: {
@@ -12,8 +13,15 @@ interface registerRequest extends Request {
   };
 }
 
+interface loginRequest extends Request {
+  body: {
+    email: string;
+    password: string;
+  };
+}
+
 export const register = async (req: registerRequest, res: Response) => {
-  let { id, fullname, email, password, isAdmin = false } = req.body;
+  let { fullname, email, password, isAdmin = false } = req.body;
   try {
     let exists =
       (await db.execute("getUSer", { email })).length === 0 ? false : true;
@@ -22,15 +30,15 @@ export const register = async (req: registerRequest, res: Response) => {
       return res.status(400).json({ error: "Wrong registration details" });
 
     let hashedPassword = await bcrypt.hash(password, 8);
-
-    await db.execute("registerUser", {
+    let id = uuidv4();
+    await db.execute("registerOrUpdateUser", {
       id,
       fullname,
       email,
-      password:hashedPassword,
+      password: hashedPassword,
       isAdmin: isAdmin,
     });
-    res.status(200).json({ info: exists });
+    res.status(200).json({ message: "user added succesfully" });
   } catch (error) {
     let message = error || "Try again later can't process the request now";
     res.status(500).json({ error: message });
