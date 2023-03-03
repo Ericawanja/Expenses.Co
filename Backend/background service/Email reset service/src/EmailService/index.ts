93f416b4307b7da7cb4d8e3d95c05f869924739b
Ericawanja <wanjaerica@gmail.com>
2023-03-03 11:18:41 +0300

    reset emailing service

---
+++ Backend/background service/Email reset service/src/EmailService/index.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import ejs from "ejs";
+import path from "path";
+import { exec } from "../helpers/dbConnect";
+import sendMail from "../helpers/Email";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const sendWelcomeEmail = async () => {
+  const emailList = await exec("resetEmailList");
+ 
+  for (let user of emailList) {
+    ejs.renderFile(
+      "Templates/reset.ejs",
+      { name: "there" },
+      async (error, html) => {
+        const message = {
+          from: process.env.FROM,
+          to: user.email,
+          subject: "testing",
+          html,
+        };
+
+        try {
+          await sendMail(message);
+
+          await exec("updateResetQueue", { email: user.email });
+        } catch (error) {
+          console.log("erro here");
+          console.log("error", error);
+        }
+      }
+    );
+  }
+};
+export default sendWelcomeEmail;
