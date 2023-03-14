d663357d15ef355535871de063459454abb07a79
Ericawanja <wanjaerica@gmail.com>
2023-03-14 22:31:57 +0300

    clients table database docs

---
+++ database/Stored procedures/Clients table SP/deleteClient.sql
@@ -0,0 +1,5 @@
+create or alter Procedure removeClient(@id varchar(100))
+AS
+BEGIN
+update clients set isDeleted = 1 where id = @id
+End
---
+++ database/Tables/Clients.sql
@@ -0,0 +1,6 @@
+create table clients(
+    id varchar(100) Primary key,
+    name varchar(100) Not null,
+    email varchar(150) Not Null,
+    location varchar(100) Not null,
+)
