59cd50b6ccd057557a05e564700c7676bfea573a
Ericawanja <wanjaerica@gmail.com>
2023-03-16 16:28:28 +0300

    authorization bug fixed

--- Backend/api logic/src/Routers/clients.ts
+++ Backend/api logic/src/Routers/clients.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express'
 import { addClient, removeClient, updateClientDetails } from '../Controllers/clients';
+import verify from '../middlewares/verify/index';
 
 const clientsRoutes = Router()
 
-clientsRoutes.post("/add", addClient)
+clientsRoutes.post("/add", verify, addClient)
 clientsRoutes.put("/:id", updateClientDetails)
 clientsRoutes.delete("/:id", removeClient)
 
